5ff0a2743c100f6eb3ddd717d0f053c077174ff2
maulina1602 <linamaulina.ml@gmail.com>
2020-04-25 17:35:59 +0800

    Tugas Kreatif perulangan Do Whie (commitan benar)

---
+++ src/tugaskreatif/perulangan/DoWhileFIX.java
@@ -0,0 +1,19 @@
+package tugaskreatif.perulangan;
+
+public class DoWhileFIX {
+    public static void main(String[] args) {
+        int SppPerBULAN = 875;
+        int SppLunas = 3500;
+
+        System.out.println("SPP Bulan Ini : Rp." + SppPerBULAN);
+        System.out.println("SPP LUNAS : Rp." + SppLunas);
+
+        do{
+            SppPerBULAN++;
+            System.out.println("Masa Perkuliahan(nabung bayar spp) : Rp." + SppPerBULAN);
+        }while (SppPerBULAN !=SppLunas);
+        System.out.println("Finale: Akhir Semester SPP LUNAS : Rp." + SppPerBULAN);
+
+    }
+}
+
