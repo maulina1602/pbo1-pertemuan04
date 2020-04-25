b0ddea57676be7538973789fc1b1d41716c42324
maulina1602 <linamaulina.ml@gmail.com>
2020-04-25 17:11:33 +0800

    Tugas Kreatif Deklarasi Array Cara 1

---
+++ src/tugaskreatif/array/DArrayCara1.java
@@ -0,0 +1,14 @@
+package tugaskreatif.array;
+
+public class DArrayCara1 {
+    public static void main(String[] args) {
+        String hari[] ={"Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+
+        System.out.println(hari.length +" Hari dalam seminggu");
+        System.out.println(hari[0] +" TUGAS PBO");
+        System.out.println(hari[1] +" KULIAH ONLINE UwU");
+        System.out.println(hari[2] +" PRAKTIKUM");
+        System.out.println(hari[5] +" & "+ hari[6] +" REBAHAN");
+
+    }
+}
